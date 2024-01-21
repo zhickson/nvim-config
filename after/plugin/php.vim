1b200e69cbb769439c2a8794d850b075c69bb524
Zachary Hickson <zhickson@thecode.co>
2024-01-22 09:11:01 +1100

    initial commit

---
+++ after/plugin/php.vim
@@ -0,0 +1,5 @@
+" Fix files with PHPCS and PHPCBF.
+
+let g:ale_linters = { 'php': ['php', 'phpcs'] }
+let g:ale_fixers = { 'php': ['phpcbf'] }
+
---
+++ lua/custom/plugins/test.lua
@@ -0,0 +1,8 @@
+-- Test copilot lua configuration file
+-- Hello world, copilot, are you awake??
+--
+-- Some other text here? Copilot, are you alive?
+return {
+  -- Copilot are you alive?
+  -- Hello?
+}
